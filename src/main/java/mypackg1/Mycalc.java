de37010865533d97881093f431f307371e0fc512
Ant2965 <antrikshbahri@gmail.com>
2023-08-25 09:45:21 +0530

    Version 1

---
+++ src/main/java/mypackg1/Mycalc.java
@@ -0,0 +1,25 @@
+package mypackg1;
+
+public class Mycalc {
+
+	
+	
+	public static int sum (int a, int b) {
+		
+		return (a+b);
+		
+	}
+	
+	
+	
+	
+	public static void main(String[] args) {
+		
+		int c = sum(3,5);
+		
+		
+		System.out.println(c);		
+
+	}
+
+}
---
+++ src/main/java/mypackg1/Myclass.java
@@ -0,0 +1,69 @@
+package mypackg1;
+
+public class Myclass {
+	void show() {
+		System.out.println("Hello Maven");
+	}
+	public static void main(String args[]) {
+		Myclass ob = new Myclass();
+		ob.show();
+		
+	}
+
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
