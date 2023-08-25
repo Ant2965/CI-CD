0ac07f9e4243ba89f3eb4857bdf65bc310a55548
Ant2965 <antrikshbahri@gmail.com>
2023-08-25 10:21:09 +0530

    Second version

--- src/main/java/mypackg1/Mycalc.java
+++ src/main/java/mypackg1/Mycalc.java
@@ -10,15 +10,19 @@ public class Mycalc {
 		
 	}
 	
+	public static int mul(int a ,int b) {
+		return(a*b);
+	}
 	
 	
 	
 	public static void main(String[] args) {
 		
 		int c = sum(3,5);
+		int d = mul(54,65);
 		
-		
-		System.out.println(c);		
+		System.out.println(c);	
+		System.out.println(d);
 
 	}
 
